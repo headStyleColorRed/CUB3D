0be8c934dbf3c527f86964e2535bd5f4e2e693a2
Rodrigo Labrador <tangerinecoder@gmail.com>
2020-05-16 22:01:01 +0200

    Adding Debug

--- Wolfenstein/main.c
+++ Wolfenstein/main.c
@@ -14,9 +14,17 @@ int main(int argc, char **argv)
 	t_map	map;
 	t_game	game;
 	
-	file_processor(argc, argv, &map);
-	start_game(&map, &game);
+	// file_processor(argc, argv, &map);
+	// start_game(&map, &game);
+    int a;
+    int b;
+    int c;
 
-	printf("Todo ok!\n");
+    a = 5;
+    b = 4;
+    c = a + b;
+
+	printf("The sum is %d\n", c);
+	
 	return (0);
 }
