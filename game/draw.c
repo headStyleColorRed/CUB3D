bc8eeb06082867ba5e3a0bb5c756eedd2df23391
Rodrigo Labrador <headstylecolorred@gmail.com>
2020-09-19 14:01:40 +0200

    added comments

--- game/draw.c
+++ game/draw.c
@@ -6,16 +6,16 @@
 /*   By: rlabrado <headstylecolorred@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/07 23:31:45 by rlabrado          #+#    #+#             */
-/*   Updated: 2020/09/19 13:56:11 by rlabrado         ###   ########.fr       */
+/*   Updated: 2020/09/19 14:01:38 by rlabrado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../library.h"
 
-void	put_pxl_to_img(t_game *game, int i)
+void	put_pxl_to_img(t_game *game, int i, int color)
 {
 	if (game->raycast.current_ray < game->window.width && i < game->window.width)
-		ft_memcpy(game->window.img_ptr + 4 * game->window.width * i + game->raycast.current_ray * 4, &game->raycast.wall_color, sizeof(int));
+		ft_memcpy(game->window.img_ptr + 4 * game->window.width * i + game->raycast.current_ray * 4, &color, sizeof(int));
 }
 
 /*
@@ -30,7 +30,7 @@ void draw_wall(t_game *game)
 	i = game->raycast.draw_start;
 	while (i <= game->raycast.draw_end)
 	{
-		put_pxl_to_img(game, i);
+		put_pxl_to_img(game, i, game->raycast.wall_color);
 		i++;
 	}
 }
@@ -38,10 +38,6 @@ void draw_wall(t_game *game)
 /*
 **	draw_floor_and_ceiling()
 **	We copy the colored pixels from the wall's start point, to the end one.
-**	
-**	
-**	
-**	
 */
 
 void draw_floor_and_ceiling(t_game *game)
@@ -53,7 +49,7 @@ void draw_floor_and_ceiling(t_game *game)
 	{
 		while (i < game->raycast.draw_start)
 		{
-			ft_memcpy(game->window.img_ptr + 4 * game->window.width * i + game->raycast.current_ray * 4, &game->raycast.celing_color, sizeof(int));
+			put_pxl_to_img(game, i, game->raycast.celing_color);
 			i++;
 		}
 	}
@@ -64,10 +60,10 @@ void draw_floor_and_ceiling(t_game *game)
 		if (game->raycast.current_ray < game->window.width && i < game->window.height)
 		{
 			while (i < game->window.height)
-			{
-				ft_memcpy(game->window.img_ptr + 4 * game->window.width * i + game->raycast.current_ray * 4, &game->raycast.floor_color, sizeof(int));
-				i++;
-			}
+		{
+			put_pxl_to_img(game, i, game->raycast.floor_color);
+			i++;
+		}
 
 		}
 	}
