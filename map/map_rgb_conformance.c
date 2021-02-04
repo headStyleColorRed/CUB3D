1590236e971bbd353c65664abe50f14816b4fe40
Rodrigo Labrador <headstylecolorred@gmail.com>
2021-02-04 20:45:23 +0100

    solved bad paremeter input

--- map/map_rgb_conformance.c
+++ map/map_rgb_conformance.c
@@ -33,22 +33,29 @@ void	is_rgb_color_number_offlimits(char **splitted_fields)
 		print_error("Third Ceiling/Floor RGB parameter is offlimits");
 }
 
-void is_valid_parameter(char *param)
+t_bool	is_valid_parameter(char *param)
 {
 	t_bool isValid;
 
+	isValid = ft_false;
 	if (ft_strlen(param) > 2)
 		print_error("Parameter is longer than 2 characters");
 
 	if (ft_strlen(param) == 1)
+		if (ft_strncmp(param, "R", 1 )== 0 || ft_strncmp(param, "S", 1 )== 0 ||
+			ft_strncmp(param, "C", 1 )== 0 || ft_strncmp(param, "F", 1 )== 0 )
+			isValid = ft_true;
+	if (ft_strlen(param) == 2)
 	{
-		if (ft_strncmp(param, "R", 1))
+		if (ft_strncmp(param, "NO", 2) == 0 || ft_strncmp(param, "SO", 2) == 0 ||
+		ft_strncmp(param, "EA", 2) == 0 || ft_strncmp(param, "WE", 2) == 0)
 			isValid = ft_true;
 	}
 
+	return isValid;
 }
 
-void	check_if_first_parameter_is_allowed(char *line)
+t_bool	check_if_first_parameter_is_allowed(char *line)
 {
 	int		i;
 	char	croppedField[5];
@@ -60,7 +67,9 @@ void	check_if_first_parameter_is_allowed(char *line)
 		i++;
 	}
 	croppedField[i] = '\0';
-	is_valid_parameter(croppedField);
+	if (is_valid_parameter(croppedField))
+		return ft_true;
+	return ft_false;
 }
 
 void	check_all_fields_are_present(t_map *map)
@@ -69,16 +78,22 @@ void	check_all_fields_are_present(t_map *map)
 	int		i;
 	int		j;
 	char	*line;
+	int		correct_fields;
+
 
 	fd = open(map->map_file, O_RDONLY);
 	i = 0;
 	j = 0;
+	correct_fields = 0;
 	while (get_next_line(fd, &line) == 1)
 	{
 		// printf("%s", line);
-		check_if_first_parameter_is_allowed(line);
+		if (check_if_first_parameter_is_allowed(line) == ft_true)
+			correct_fields++;
 
 		free(line);
 	}
 	close(fd);
+	if (correct_fields != 8)
+		print_error("A Field description was wrong");
 }
